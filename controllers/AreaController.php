3e4c8a39e18455a3401a8daa8c738004670e0690
Morne Oosthuizen <morneoos@gmail.com>
2016-04-13 01:21:04 +0200

    Updates

--- controllers/AreaController.php
+++ controllers/AreaController.php
@@ -11,18 +11,18 @@ use Phalcon\Mvc\Controller;
 
 class AreaController extends ControllerBase
 {
-	
+
 	public function setAction() {
 		error_log("[Controller][Area/Set]");
-		
+
 		$obj = json_decode($this->request->getRawBody());
-		
+
 		$area = $obj->area;
-		
+
 		$data = array();
-		
+
 		$area_new = \CoderSet\Models\RestaurantBranchArea::findFirst('id = ' . $area->id);
-		
+
 		if ($area_new) {
 			error_log("here : " . json_encode($area));
 			$area_new->restaurant_branch_id = $area->restaurant_branch_id;
@@ -30,92 +30,98 @@ class AreaController extends ControllerBase
 			$area_new->status = 1;
 			$area_new->save();
 		}
-		
+
 		$result = array(
 				'status' => true,
 				'data' => $data
 		);
-			
+
 		$this->response->setContent(json_encode($result));
 	}
-	
+
 	public function delAction() {
 		error_log("[Controller][Area/Del]");
-		
+
 		$obj = json_decode($this->request->getRawBody());
-		
+
 		$area = $obj->area;
-		
+
 		$data = array();
-		
+
 		$area_new = \CoderSet\Models\RestaurantBranchArea::findFirst('id = ' . $area->id);
-		
+
 		if ($area_new) {
 			$area_new->status = 0;
 			$area_new->save();
 		}
-		
+
 		$result = array(
 				'status' => true,
 				'data' => $data
 		);
-			
+
 		$this->response->setContent(json_encode($result));
 	}
-	
+
 	public function addAction() {
 		error_log("[Controller][Area/Add]");
 
 		$obj = json_decode($this->request->getRawBody());
-		
+
 		$data = array();
 
 		$area = new \CoderSet\Models\RestaurantBranchArea();
-		
+
 		$area->restaurant_branch_id = $obj->restaurant_branch_id;
 		$area->name = 'new area';
+		$area->status = '1';
 		$area->save();
-		
+
 		$result = array(
 				'status' => true,
 				'data' => $data
 		);
-		 
+
 		$this->response->setContent(json_encode($result));
-		
+
 	}
-	
+
 	public function getAction() {
 		error_log("[Controller][Area/Get]");
-	
+
 		$obj = json_decode($this->request->getRawBody());
-	
+
 		$data = array();
-	
+
 		$area = \CoderSet\Models\RestaurantBranchArea::find('restaurant_branch_id = ' . $obj->restaurant_branch_id . ' AND status = \'1\'');
-		
+
+		$tmp_holder = array();
+
 		foreach ($area as $a) {
-			
-			
+
 			if($obj->include_tables) {
+				$a->RestaurantBranchAreaTable;
 				$tables = array();
 				foreach($a->RestaurantBranchAreaTable as $t) {
 					$tables[] = $t;
 				}
-				
-				$a->RestaurantBranchAreaTable = $tables;
+				$tmp_holder[] = $tables;
 			}
-			
+
+			$a->fuckme = "Asdf";
+
+			//$a->RestaurantBranchAreaTable = $tmp_holder;
 			$data[] = $a;
+
 		}
-	
+
 		$result = array(
 				'status' => true,
 				'data' => $data
 		);
-			
+
 		$this->response->setContent(json_encode($result));
-	
+
 	}
-	
+
 }
